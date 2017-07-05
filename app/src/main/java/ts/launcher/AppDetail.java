84950212fa1fc888f44ae084e650e3ccec6af720
azbe <tudorpetreradu@gmai.com>
2017-07-05 18:08:47 +0300

    Second commit.

---
+++ app/src/main/java/ts/launcher/AppDetail.java
@@ -0,0 +1,9 @@
+package ts.launcher;
+import android.graphics.drawable.Drawable;
+
+public class AppDetail
+{
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
---
+++ app/src/main/java/ts/launcher/HomeActivity.java
@@ -0,0 +1,20 @@
+package ts.launcher;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void showApps(View v){
+        Intent i = new Intent(this, AppListActivity.class);
+        startActivity(i);
+    }
+}
